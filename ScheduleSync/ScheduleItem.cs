3296435de992c20f4173285eda849b87d5c585d1
Erwin Suwitoandojo <erwin.suwitoandojo@outlook.com>
2020-08-27 15:51:28 +0700

    Modified SetIntakePage to include tutorial group and foreign/local student

---
+++ ScheduleSync/ScheduleItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleSync
+{
+    public class ScheduleItem
+    {
+        public string INTAKE { get; set; }
+        public string MODID { get; set; }
+        public string DAY { get; set; }
+        public string LOCATION { get; set; }
+        public string ROOM { get; set; }
+        public string LECTID { get; set; }
+        public string NAME { get; set; }
+        public string SAMACCOUNTNAME { get; set; }
+        public string DATESTAMP { get; set; }
+        public string DATESTAMP_ISO { get; set; }
+        public string TIME_FROM { get; set; }
+        public string TIME_TO { get; set; }
+        public string GROUPING { get; set; }
+        public string COLOR { get; set; }
+    }
+}

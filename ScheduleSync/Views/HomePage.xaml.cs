eb61197dc63ef26263eb4d43c53687ba0f5f1bf3
Erwin Suwitoandojo <30384397+ErwinSuwito@users.noreply.github.com>
2020-12-02 13:59:45 +0700

    Removed check before downloading new schedule

--- ScheduleSync/Views/HomePage.xaml.cs
+++ ScheduleSync/Views/HomePage.xaml.cs
@@ -56,19 +56,8 @@ namespace ScheduleSync.Views
             string tutorialGroup = localSettings.Values["TutorialGroup"].ToString();
             bool.TryParse(localSettings.Values["IsLocalStudent"].ToString(), out bool isLocalStudent);
 
-            bool isLatestScheduleSynced = false;
-
-            if (localSettings.Values["SyncedUntilDate"] != null)
-            {
-                string syncedUntil = localSettings.Values["SyncedUntilDate"].ToString();
-                bool.TryParse(localSettings.Values[syncedUntil].ToString(), out isLatestScheduleSynced);
-            }
-
-            if (!isLatestScheduleSynced)
-            {
-                await da.GetSchedule();
-                await da.ExtractGZip();
-            }
+            await da.GetSchedule();
+            await da.ExtractGZip();
 
             List<Schedule> completeTimetable = await da.ReadAndParseSchedule();
             List<Schedule> timetable = await da.FilterTimetablev2(completeTimetable, intakeCode, tutorialGroup, isLocalStudent);
